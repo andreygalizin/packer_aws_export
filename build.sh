28a2285bf90687e63f54e0b581628ac2b2756ea4
andrey-galizin <andrey.galizin@gmail.com>
2021-08-16 20:00:52 -0400

    switch to local packer

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # export CUSTOM_AMI_NAME=your-ami-name
 export CUSTOM_AMI_NAME=sher-ami
-packer build ami-create.json
+./packer build ami-create.json
 
 # AMI_ID=$(aws ec2 describe-images --owners self --filters "Name=name,Values=${CUSTOM_AMI_NAME}" | grep ImageId | cut -f4 -d'"')
 
